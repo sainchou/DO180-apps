ff17133ff659c7e394549ae71fe476735ec4ca7f
Student User <student@workstation.lab.example.com>
2022-06-01 01:25:10 -0400

    updated app

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -3,4 +3,25 @@ print "Hello, World! php version is " . PHP_VERSION . "\n";
 
 print "Hello Stanley!";
 
+
+
+    echo "HTTP_HOST：".$_SERVER['HTTP_HOST']."<hr />";
+
+    echo "SERVER_NAME：".$_SERVER['SERVER_NAME']."<hr />";
+
+        echo "REQUEST_URI：".$_SERVER['REQUEST_URI']."<hr />";
+
+        echo "PHP_SELF：".$_SERVER['PHP_SELF']."<hr />";
+
+	    echo "QUERY_STRING：".$_SERVER['QUERY_STRING']."<hr />";
+
+
+
+
+
+
+
+
+
+
 ?>
